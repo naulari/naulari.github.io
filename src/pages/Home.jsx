024f79e8f21e1d766b63187f116de32c301a09d5
naulari <jalvarr@outlook.com>
2026-05-03 21:14:29 -0700

    finished(?) creating all tailwind css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen container">
+
+        {/* {Theme Toggle} */}
+
+        {/* Background Effects */}
+
+        {/* NavBar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+        </div>
+    );
+};
