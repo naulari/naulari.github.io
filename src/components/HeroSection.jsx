e094a49e35c8a3aea2d3a1023ec0191e1d99cb8c
naulari <jalvarr@outlook.com>
2026-05-05 14:00:02 -0700

    Hero Section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,33 @@
+import { ArrowDown } from "lucide-react";
+
+export const HeroSection = () => {
+    return (
+        <section id="hero" 
+            className="relative min-h-screen flex flex-col items-center justify-center px-4">
+            
+            <div className="container mx-w-4xl mx-auto text-center z-10">
+                <div className="space-y-6">
+                    <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+                       <span className="opacity-0 animate-fade-in"> Hi, I'm</span> 
+                       <span className="text-primary opacity-0 animate-fade-in-delay-1"> Juan</span>
+                       <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Alvarracin Naulaguari</span>
+                    </h1>
+
+                    <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur dolore explicabo, enim harum inventore omnis impedit sint tempore voluptatibus aliquid, minima porro esse expedita error cumque nemo in sequi ipsa!
+                    </p>
+
+                    <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+                        <a href="#projects" className="cosmic-button">
+                            View My Work
+                        </a>
+                    </div>
+                </div>
+            </div>
+        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+            <span className="text-sm text-muted-foreground mb-2"> Scroll </span>
+            <ArrowDown className="h-5 w-5 text-primary"/>
+        </div>
+        </section>
+    );
+}
