7fef4791c22201278d1891070a3f12875ca4ecab
naulari <jalvarr@outlook.com>
2026-05-07 21:53:54 -0700

    Project Section

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,98 @@
+import { ArrowRight, ExternalLink } from "lucide-react";
+
+const projects = [
+    {
+        id: 1,
+        title: "Project 1",
+        description: "Lorem ipsum dolor sit amet.",
+        image: "/projects/project1.png",
+        tags: ["React", "TailwindCSS", "Supabase"],
+        demoURL: "#",
+        githubURL: "#"
+    },
+    {
+        id: 2,
+        title: "Project 2",
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        image: "/projects/project2.png",
+        tags: ["TypeScript", "D3.js", "Next.js"],
+        demoURL: "#",
+        githubURL: "#"
+    },
+    {
+        id: 3,
+        title: "Project 3",
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        image: "/projects/project3.png",
+        tags: ["React", "Node.js", "Stripe"],
+        demoURL: "#",
+        githubURL: "#"
+    }
+];
+
+export const ProjectSection = () => {
+    return (
+        <section id="projects" className="py-24 px-4 relative">
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center"> Featured <span className="text-primary"> Projects </span> </h2>
+
+                <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto ">
+                    Here are some of my pojects. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, possimus.
+                </p>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {projects.map((project,key)=>(
+                        <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+                            <div className="h-48 overflow-hidden">
+                                <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
+                            </div>
+
+                            <div className="p-6">
+                                <div className="flex flex-wrap gap-2 mb-4">
+                                    {project.tags.map((tag)=>(
+                                        <span className="px-2 py-1 border text-xs font-medium rounded-full bg-primary/30 text-secondary-foreground">
+                                            {tag}
+                                        </span>
+                                    ))}
+                                </div>
+                            
+
+                                <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
+                                <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
+                                <div className="flex justify-between items-center">
+                                    <div className="flex space-x-3">
+                                        <a href={project.demoURL} 
+                                            className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                            target="_blank">
+                                            <ExternalLink size={20}/>
+                                        </a>
+                                        <a href={project.githubURL} 
+                                            className="text-foreground/80 hover:text-primary transition-colors duration-300" 
+                                            target="_blank">
+                                            <svg 
+                                                role="img" 
+                                                viewBox="0 0 24 24" 
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="currentColor"
+                                                width="20"
+                                                height="20">
+                                                <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
+                                            </svg>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="text-center mt-12">
+                    <a className="cosmic-button w-fit flex items-center mx-auto gap-2"
+                        href="https://github.com/naulari"
+                        target="_blank">
+                        Check My Github <ArrowRight size={16}/>
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
+}
