e0004ff0de09dca8ac0a666a21259595a5b35105
naulari <jalvarr@outlook.com>
2026-05-10 22:52:33 -0700

    Changed website title and fixed some parts in the contacts section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -53,12 +53,14 @@ export const ContactSection = () => {
                     <div className="space-y-8">
                         <h3 className="text-2xl font-semibold mb-6"> Contact Information</h3>
 
-                        <div className="space-y-6 justify-center">
-                            <div className="flex items-start space-x-4">
-                                <div className="p-3 rounded-full bg-primary/10">
-                                    <Mail className="h-6 w-6 text-primary" />
+                        <div className="space-y-6">
+                            <div className="grid grid-cols-[auto_1fr] items-start">
+                                <div className="p-3 rounded-full bg-primary/10 hover:scale-[1.1]">
+                                    <a href="mailto:jalvarr@outlook.com">
+                                        <Mail className="h-6 w-6 text-primary" />
+                                    </a>
                                 </div>
-                                <div>
+                                <div className="pr-12">
                                     <h4 className="font-medium"> Email</h4>
                                     <a href="mailto:jalvarr@outlook.com"
                                         className="text-muted-foreground hover:text-primary transition-colors">
@@ -66,11 +68,13 @@ export const ContactSection = () => {
                                     </a>
                                 </div>
                             </div>
-                            <div className="flex items-start space-x-4">
-                                <div className="p-3 rounded-full bg-primary/10">
-                                    <Phone className="h-6 w-6 text-primary" />
+                            <div className="grid grid-cols-[auto_1fr] items-start">
+                                <div className="p-3 rounded-full bg-primary/10 hover:scale-[1.1]">
+                                    <a href="tel:+19144268262">
+                                        <Phone className="h-6 w-6 text-primary" />
+                                    </a>
                                 </div>
-                                <div>
+                                <div className="pr-12">
                                     <h4 className="font-medium"> Phone</h4>
                                     <a href="tel:+19144268262"
                                         className="text-muted-foreground hover:text-primary transition-colors">
@@ -78,15 +82,15 @@ export const ContactSection = () => {
                                     </a>
                                 </div>
                             </div>
-                            <div className="flex items-start space-x-4">
+                            <div className="grid grid-cols-[auto_1fr] items-start">
                                 <div className="p-3 rounded-full bg-primary/10">
                                     <MapPin className="h-6 w-6 text-primary" />
                                 </div>
-                                <div>
+                                <div className="pr-12">
                                     <h4 className="font-medium"> Location</h4>
-                                    <a  className="text-muted-foreground hover:text-primary transition-colors">
+                                    <p  className="text-muted-foreground hover:text-primary transition-colors">
                                         Los Angeles, CA
-                                    </a>
+                                    </p>
                                 </div>
                             </div>
                         </div>
@@ -97,47 +101,51 @@ export const ContactSection = () => {
                                 <a href="https://linkedin.com/in/jalvarr/"
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    aria-label="Link to my LinkedIn Profile">
+                                    aria-hidden="true"
+                                    aria-label="Link to my LinkedIn Profile"
+                                    className="hover:text-primary">
                                     <LinkedIn/>
                                 </a>
                                 <a href="https://instagram.com/naulari"
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    aria-label="Link to my Instagram Profile">
+                                    aria-hidden="true"
+                                    aria-label="Link to my Instagram Profile"
+                                    className="hover:text-primary">
                                     <Instagram/>
                                 </a>
                             </div>
                        </div>
                     </div>
                 
-                    <div className="bg-card p-8 rounded-lg shadow-xs" 
-                        onSubmit={handleSubmit}>
+                    <div className="bg-card p-8 rounded-lg shadow-xs">
                         <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
-                        <form className="space-y-6">
+                        <form className="space-y-6" onSubmit={handleSubmit}>
                             <div>
                                 <label htmlFor="name"
-                                    className="block text-sm font-medium b-2"> Your Name</label>
+                                    className="block text-sm font-medium mb-2"> Your Name</label>
                                 <input id="name" 
                                     required 
                                     type="text" 
-                                    className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
+                                    className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary"
                                     placeholder="Juan Alvarracin"/>
                             </div>
                             <div>
                                 <label htmlFor="email"
-                                    className="block text-sm font-medium b-2"> Your Email</label>
+                                    className="block text-sm font-medium mb-2"> Your Email</label>
                                 <input id="email" 
                                     required 
                                     type="email" 
-                                    className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
+                                    className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary"
                                     placeholder="jalvarr@outlook.com"/>
                             </div>
                             <div>
                                 <label htmlFor="message"
-                                    className="block text-sm font-medium b-2"> Your Message</label>
+                                    className="block text-sm font-medium mb-2"> Your Message</label>
                                 <textarea id="message" 
-                                    required 
-                                    className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary resize-none"
+                                    required
+                                    rows={5} 
+                                    className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary resize-none"
                                     placeholder="Hello, I'd like to talk about..."/>
                             </div>
                             <button type="submit" 
@@ -145,8 +153,11 @@ export const ContactSection = () => {
                                 className={cn("cosmic-button w-full flex items-center justify-center gap-2",
                                     
                                 )}>
-                                {isSubmitting ? "Sending...": "Send Message"}
-                                <Send size={16}/>
+                                {isSubmitting ? ("Sending...")
+                                :( <>
+                                    Send Message <Send size={16}/>
+                                </>)}
+                               
                             </button>
                         </form>
                     </div>
