e3bf89ae41a99161f21dc3be1b286095ac809da5
naulari <jalvarr@outlook.com>
2026-06-25 01:21:32 -0700

    Email Message Functionality Added

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -3,6 +3,8 @@ import { cn } from "@/lib/utils";
 import { useToast } from "../hooks/use-toast";
 import { useState } from "react";
 
+
+
 const LinkedIn = () => (
     <svg role="img" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 sm:w-6 sm:h-6" fill="currentColor" viewBox="0 0 16 16">
     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
@@ -20,18 +22,70 @@ export const ContactSection = () => {
     const { toast } = useToast();
     const [isSubmitting, setIsSubmitting] = useState(false);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         setIsSubmitting(true);
 
-        setTimeout(()=>{
+        // setTimeout(()=>{
+        //     toast({
+        //         title: "Message Sent!",
+        //         description: "Thank you for your message. I'll get back to you soon."
+        //     });
+        //     setIsSubmitting(false);
+        // }, 1500)
+
+        try{
+            const formData = new FormData(e.target);
+
+            formData.append("access_key", import.meta.env.VITE_WEB3FORMS_ACCESS_KEY);
+
+            formData.append(
+            "subject",
+            "New Portfolio Contact Form Submission"
+        );
+
+            const object = Object.fromEntries(formData);
+            const json = JSON.stringify(object);
+
+            const response = await fetch(
+                "https://api.web3forms.com/submit",
+                {
+                    method: "POST",
+                    headers:{
+                        "Content-Type" : "application/json",
+                        Accept: "application/json",
+                    },
+                    body: json,
+                }
+            );
+            const result = await response.json();
+
+            if (result.success){
+                toast({
+                    title: "Message Sent!",
+                    description:
+                    "Thank you for your message. I'll get back to you soon.",
+                });
+                e.target.reset();
+            }else{
+                toast({
+                    title: "Error",
+                    description: 
+                        "Failed to send message. Please try again.",
+                    variant: "destructive",
+                });
+            }
+        } catch (error){
             toast({
-                title: "Message Sent!",
-                description: "Thank you for your message. I'll get back to you soon."
+                title: "Error",
+                description:
+                    "Something went wrong. Please try again later.",
+                variant: "destructive",
             });
+        } finally {
             setIsSubmitting(false);
-        }, 1500)
+        }
 
         
     };
@@ -125,6 +179,7 @@ export const ContactSection = () => {
                                 <label htmlFor="name"
                                     className="block text-sm font-medium mb-2"> Your Name</label>
                                 <input id="name" 
+                                    name="name"
                                     required 
                                     type="text" 
                                     className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary"
@@ -133,7 +188,8 @@ export const ContactSection = () => {
                             <div>
                                 <label htmlFor="email"
                                     className="block text-sm font-medium mb-2"> Your Email</label>
-                                <input id="email" 
+                                <input id="email"
+                                    name="email" 
                                     required 
                                     type="email" 
                                     className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary"
@@ -143,6 +199,7 @@ export const ContactSection = () => {
                                 <label htmlFor="message"
                                     className="block text-sm font-medium mb-2"> Your Message</label>
                                 <textarea id="message" 
+                                    name="message"
                                     required
                                     rows={3} 
                                     className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary resize-none"
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     return (
         <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
             <p className="text-sm text-muted-foreground"> 
-                &copy; {new Date().getFullYear()} naulari.github.io, All rights reserved. 
+                &copy; {new Date().getFullYear()} jalvarr.com, All rights reserved. 
             </p>
             <button onClick={()=>document.getElementById("hero")?.scrollIntoView({behavior:"smooth"})} className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors"> 
                 <ArrowUp size={20}/>
